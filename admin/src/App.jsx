a8219c505a622a9eb76ed561a40720bf87ad0034
Guilherme Pacheco De Souza <guilhermesouza23022006@gmail.com>
2025-09-01 23:00:45 -0300

    first commit

---
+++ admin/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>somente devs da parte de administacao mecham na pasta de admin pfv.</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>devs de front end mecham somente na pasta do front pfv.</h1>
+    </div>
+  )
+}
+
+export default App
