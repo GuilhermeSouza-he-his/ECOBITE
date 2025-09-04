bc7cd7b636468b366f327df4168e3eb5d7ca6212
Maike Lohan P. Louzeiro <maikelouzeiro@gmail.com>
2025-09-04 10:24:33 -0300

    adicionando template principal e pagina de login ainda nao fiznalizadasP

---
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -0,0 +1,35 @@
+import React, { useContext } from 'react'
+import "./FoodItem.css"
+import { assets } from '../../assets/assets'
+import { StoreContext } from '../../context/StoreContext'
+
+const FoodItem = ({id, name, price, description, image}) => {
+
+  const {cartItem,addToCart,removeFromCart,url} = useContext(StoreContext)
+
+  return (
+    <div className='food-item'>
+      <div className='food-item-img-container'>
+        <img className='food-item-image' src={url+"/images/"+image}/>
+        {!cartItem[id]
+          ?<img className='add' onClick={()=>addToCart(id)} src={assets.add} alt=''/>
+          :<div className='food-item-count'>
+              <img onClick={()=>removeFromCart(id)} src={assets.add_icon_red}/>
+              <p>{cartItem[id]}</p>
+              <img onClick={()=>addToCart(id)}src={assets.add_icon_green}/>
+          </div>
+        }
+      </div>
+      <div className='food-item-info'>
+        <div className='food-item-name-rating'>
+            <p>{name}</p>
+            <img src={assets.rating_stars}/>
+        </div>
+      <p className='food-item-desc'>{description}</p>
+      <p className='food-item-price'>${price}</p>
+      </div>
+    </div>
+  )
+}
+
+export default FoodItem
