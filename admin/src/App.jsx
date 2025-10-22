ac341fc735c6c6457f5913ab0087047a97bef29b
Guilherme Souza <guilhermesouza23022006@gmail.com>
2025-10-22 14:03:42 -0300

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './components/Navbar/NavBar'
+import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/SideBar/SideBar'
 import { Route, Routes } from 'react-router-dom'
 import Add from './pages/Add/Add'
