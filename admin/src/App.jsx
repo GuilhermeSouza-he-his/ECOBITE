e1dd68ab243953f3a5d3f99b0a31d59900ce2a6e
Guilherme Souza <guilhermesouza23022006@gmail.com>
2025-11-27 07:48:22 -0300

    Change API URL from production to local

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "https://ecobite-s6ni.onrender.com"
+  const url = "http://127.0.0.1:4000"
 
   return (
     <div>
