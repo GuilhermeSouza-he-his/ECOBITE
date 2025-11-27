837f7b20634c8b52a6241da7520abb3319719888
Guilherme Souza <guilhermesouza23022006@gmail.com>
2025-11-27 07:49:26 -0300

    Change API URL to local development server

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) =>{
 
     const [cartItem, setCartItem] = useState({});
-    const url = 'https://ecobite-s6ni.onrender.com'
+    const url = 'http://127.0.0.1:4000'
     const [token,setToken] = useState("")
     const [food_list, setFoodList] = useState([])
     
